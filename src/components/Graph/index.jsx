d88947f11eb2f36ff7661d9270abba808127b601
Daniel-Alencar <danielalencar746@gmail.com>
2023-10-24 11:51:42 -0300

    Treinamento

--- src/components/Graph/index.jsx
+++ src/components/Graph/index.jsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.css';
 
-export function Graph({ name, image }) {
+export function Graph({ name, image, width }) {
   return(
     <div className={styles.graphContainer}>
       <p className={styles.name}>{name}</p>
-      <img src={image} alt="graph" className={styles.graph} />
+      <img src={image} alt="graph" className={styles.graph} 
+        style={width ? { width: width } : {}}
+      />
     </div>
   )
 }
--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -4,16 +4,36 @@ import { ProgressBar } from '../../components/ProgressBar';
 import styles from './styles.module.css';
 
 import { userName } from '../../settings';
+import { Graph } from '../../components/Graph';
+
+import GraphImage from '../../assets/results/importancia_das_variaveis.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Results() {
 
   const href = '/results';
   const progress = 4;
 
+  const navigate = useNavigate();
+  const handleTo = (URL) => {
+    navigate(URL);
+  }
+
   return(
     <>
       <UserBar name={userName}/>
       <ProgressBar progressNumber={progress}/>
+
+      <div className={styles.container}>
+        <Graph image={GraphImage} width={600}/>
+
+        <button 
+          className={styles.button}
+          onClick={() => handleTo('/results')}
+        >
+          Próximo
+        </button>
+      </div>
     </>
   )
 }
