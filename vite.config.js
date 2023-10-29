e267282dd44774e71e9076d0bbef1431d3f9a022
Daniel-Alencar <danielalencar746@gmail.com>
2023-10-29 15:30:29 -0300

    ajustes para docker

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    // needed for the Docker Container port mapping to work
+    host: true,
+    strictPort: true,
+    // you can replace this port with any port
+    port: 5173, 
+  }
 })
