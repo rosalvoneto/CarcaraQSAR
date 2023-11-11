c72ece31151d821b4c64f66323aeda5d6d6d6285
Daniel-Alencar <danielalencar746@gmail.com>
2023-11-11 16:43:43 -0300

    Primeiro método de autenticação

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -1,13 +1,41 @@
-from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     return render(request, "api/user.html")
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'api/register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        pais = request.POST.get('pais')
+        departamento = request.POST.get('departamento')
+
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            return render(request, 'api/register.html', {
+                "message": "Já existe um usuário com esse email!"
+            })
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        user.save()
+
+        return render(request, 'api/login.html')
 
 def login_view(request):
     if request.method == "POST":
