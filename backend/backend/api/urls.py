bd82d279e98e4ca90bb4f77d563b2cee6bdab0cd
Daniel-Alencar <danielalencar746@gmail.com>
2023-11-11 15:29:20 -0300

    Ajeitando api

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('post', views.post, name='post'),
+  path("", views.index, name="index"),
+  path("login", views.login_view, name="login"),
+  path("logout", views.logout_view, name="logout")
 ]
--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -1,14 +1,31 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
-
-from django.http import HttpResponse, JsonResponse
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-  json = {
-    "token": "lknsflnfkadgnldfnbskdjsgnsfgns"
-  }
-  return JsonResponse(json)
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "api/user.html")
 
-def post(request):
-  json = request.data
-  return JsonResponse(json)
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "api/login.html", {
+                "message": "Invalid credentials."
+            })
+    else:
+        return render(request, "api/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "api/login.html", {
+        "message": "Logged out."
+    })
