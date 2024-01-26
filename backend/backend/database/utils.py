e1e9713c5d472e195d84776b7da6ade7802ff0ae
Daniel-Alencar <danielalencar746@gmail.com>
2024-01-26 14:16:47 -0300

    Lógica de pre-processing

--- backend/backend/database/utils.py
+++ backend/backend/database/utils.py
@@ -4,6 +4,8 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 from io import StringIO, BytesIO
 import base64
 
+from padelpy import from_smiles
+
 def getHistogramImage(array, num_bins):
   # Fazer cálculo de Histograma
   # Calcule o histograma
@@ -61,3 +63,75 @@ def getBoxPlotImage(array):
   plt.clf()
 
   return boxPlot_base64
+
+def get_line_descriptors(akeys, adescriptors):
+  l = ''
+  for k in akeys:
+    if len(l) == 0:
+      l = adescriptors[k]
+    else:
+      l = l + ';' + adescriptors[k]
+  return l
+
+def convertSmilesToCSV():
+  smiles = "N1(C(=O)c2c(C1=O)cccc2)CC1N(C(=O)N(CC)CC)CCc2c1cccc2"
+  descriptors = from_smiles(smiles, 'output.csv', True)
+  print(descriptors)
+
+# convertSmilesToCSV()
+
+
+
+
+
+# from padelpy import padeldescriptor
+# import pandas as pd
+# import tempfile
+# import os
+
+# # Função para gerar descritores usando padelpy
+# def gerar_descritores_smiles(smiles, output_file):
+#     # Configurações do padelpy
+#       # Substitua pelo caminho real para o executável padel-descriptor
+#     padel_exe = '/caminho/para/padel-descriptor'
+#     padel_options = '-k -descriptortypes fingerprint -retainorder -fingerprints ' + output_file
+
+#     # Criar um arquivo temporário com a string SMILES
+#     with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+#       temp_file.write(smiles)
+#       temp_smiles_file = temp_file.name
+
+#     try:
+#       # Executar padeldescriptor
+#       padeldescriptor(
+#         mol_dir=temp_smiles_file, d_file=output_file, 
+#         threads=1, log=True,
+#         # descriptors=True, detectar_3D=False,
+#         # standardizer=False,
+#         # padel_path=padel_exe, padel_options=padel_options
+#       )
+
+#     except Exception as e:
+#       # Capturar e imprimir a exceção
+#       print(f"Erro ao gerar descritores: {e}")
+
+#     finally:
+#         # Remover o arquivo temporário
+#         os.remove(temp_smiles_file)
+
+# # String SMILES fornecida
+# smiles_string = "N1(C(=O)c2c(C1=O)cccc2)CC1N(C(=O)N(CC)CC)CCc2c1cccc2"
+
+# # Gerar descritores e salvar em um arquivo CSV
+# output_csv = 'output.csv'
+# gerar_descritores_smiles(smiles_string, output_csv)
+
+# # Ler o arquivo CSV gerado
+# try:
+#     df = pd.read_csv(output_csv)
+#     # Visualizar o DataFrame
+#     print(df)
+# except pd.errors.EmptyDataError:
+#     print("O arquivo CSV está vazio.")
+# except pd.errors.ParserError:
+#     print("Erro ao analisar o arquivo CSV.")
