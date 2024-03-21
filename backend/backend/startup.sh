3f1ae255930721f83bb8f17bef0711d27afc7480
Daniel-Alencar <danielalencar746@gmail.com>
2024-03-21 10:29:05 -0300

    Mudança em requirements por causa do vagrant

--- backend/backend/startup.sh
+++ backend/backend/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 manage.py migrate && python3 manage.py collectstatic --no-input && gunicorn --workers 2 backend.wsgi
+python3 manage.py migrate && python3 manage.py collectstatic --no-input && gunicorn --bind 0.0.0.0:8000 --workers 2 backend.wsgi
