d38ed8b98c5074a033eec9c7adb8a62b217e3fa8
Daniel-Alencar <danielalencar746@gmail.com>
2024-06-25 12:57:34 -0300

    feat: adicionando start.sh

--- start.sh
+++ start.sh
@@ -1,40 +1,9 @@
 #!/bin/bash
 
-# Diretório do frontend
-FRONTEND_DIR="frontend"
-# Comando para iniciar o servidor de desenvolvimento do React
-FRONTEND_INSTALL_CMD="npm install"
-FRONTEND_START_CMD="npm run dev"
+# Obtém o diretório do script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-# Diretório do backend
-BACKEND_DIR="backend/backend"
-# Comando para iniciar o servidor de desenvolvimento do Django
-BACKEND_START_CMD="python3 manage.py runserver 0.0.0.0:8000"
+# Muda para o diretório do script
+cd "$SCRIPT_DIR"
 
-# Função para iniciar o frontend
-start_frontend() {
-  echo "Iniciando o frontend..."
-  cd "$FRONTEND_DIR"
-  $FRONTEND_INSTALL_CMD
-  $FRONTEND_START_CMD
-}
-
-# Função para iniciar o backend
-start_backend() {
-  echo "Iniciando o backend..."
-  cd "$BACKEND_DIR"
-  pip3 install -r requirements.txt
-  $BACKEND_START_CMD
-}
-
-# Inicia o frontend em um processo separado
-start_frontend &
-FRONTEND_PID=$!
-
-# Inicia o backend em um processo separado
-start_backend &
-BACKEND_PID=$!
-
-# Espera que os processos terminem
-wait $FRONTEND_PID
-wait $BACKEND_PID
+docker-compose up
