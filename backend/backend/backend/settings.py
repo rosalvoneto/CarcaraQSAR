f653188496231692239769d33d9c5800d218714e
Daniel-Alencar <danielalencar746@gmail.com>
2024-08-02 20:41:28 -0300

    feat: Configuração celery in production

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -280,10 +280,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Configurações do Celery
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 # URL do broker de mensagens
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
 # URL para resultados das tarefas (opcional)
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Outras configurações do Celery, se necessário
 CELERY_ACCEPT_CONTENT = ['json']
--- backend/backend/startup.sh
+++ backend/backend/startup.sh
@@ -1,3 +1,5 @@
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
 gunicorn --bind 0.0.0.0:8000 --workers 2 --timeout 15000 backend.wsgi
+
+celery -A backend worker -l info
