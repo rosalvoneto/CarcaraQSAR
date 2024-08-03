3cb273965193c76f39d3baa9955d6b9dd1abbf93
Daniel-Alencar <danielalencar746@gmail.com>
2024-08-03 10:00:08 -0300

    feat: Configuração celery in production

--- backend/backend/startup.sh
+++ backend/backend/startup.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+
+# Inicializa o Redis
+redis-server &
+
+# Inicializa o servidor Django
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
 gunicorn --bind 0.0.0.0:8000 --workers 2 --timeout 15000 backend.wsgi
 
+# Inicializa o Celery worker
 celery -A backend worker -l info
