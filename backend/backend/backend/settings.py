48ad8d3d76bdebcfeed1d2a45d48d4693491c138
Daniel-Alencar <danielalencar746@gmail.com>
2024-08-10 11:51:30 -0300

    feat: adding databases

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -202,7 +202,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=90),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS":True,
     "BLACKLIST_AFTER_ROTATION":True,
